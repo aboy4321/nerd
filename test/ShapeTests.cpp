903b4b34ab5d14025d82587ef98e600c277357a3
aboy4321 <awboyce630@gmail.com>
2026-08-11 17:02:34 -0400

    shape file test added

--- test/ShapeTests.cpp
+++ test/ShapeTests.cpp
@@ -5,6 +5,13 @@ using namespace nerd;
 
 void test_shape() {
   Shape shape{2, 3, 4};
+  assert(shape[0] == 2);
+  assert(shape[1] == 3);
+  assert(shape[2] == 4);
+  assert(shape.rank() == 3);
+  assert(shape.size() == 24);
+  assert(!(shape.empty()));
+  shape.add(0, 2);
   std::cout << shape << std::endl;
 }
 
