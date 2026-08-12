95b3c4fed7d114eb0e79156057e89a20d23e200c
aboy4321 <awboyce630@gmail.com>
2026-08-11 20:14:42 -0400

    added openmp to matmult, it is used in latest result

--- core/TensorLA.h
+++ core/TensorLA.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Tensor.h>
+#include <vector>
 #include <cassert>
 
 namespace nerd {
@@ -9,15 +10,26 @@ template <typename Type>
 Tensor<Type> matmult(const Tensor<Type>& A, const Tensor<Type>& B) {
   assert(A.ndim() == 2 && B.ndim() == 2);
   assert(A.get_shape(1) == B.get_shape(0));
+  std::size_t inner_dim = B.get_shape(0);
   std::size_t A_rows = A.get_shape(0);
   std::size_t B_cols = B.get_shape(1);
-  Tensor<Type> res(Shape{A_rows, B_cols});
+  Tensor<Type> res(Shape{A_rows, B_cols}, Type{0});
+  std::vector<Type> buffer(B_cols, 0);
   for (std::size_t i = 0; i < A_rows; ++i) {
-    for (std::size_t k = 0; k < B.get_shape(0); ++k) {
-      Type a_ik = A(i, k);
+    
+    std::fill(buffer.begin(), buffer.end(), Type{0});
+
+    for (std::size_t k = 0; k < inner_dim; ++k) {
+      Type a_ik = A(i, k); 
+      
+      #pragma omp simd
       for (std::size_t j = 0; j < B_cols; ++j) {
-        res(i, j) += a_ik * B(k, j);
+        buffer[j] += a_ik * B(k, j);
       }
+    }
+
+    for (std::size_t j = 0; j < B_cols; ++j) {
+      res(i, j) = buffer[j];
     }
   }
   return res;
